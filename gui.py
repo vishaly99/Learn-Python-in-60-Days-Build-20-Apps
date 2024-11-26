1c4ca206fbcab1bcd48df1a7facf445d35014cc6
vishaly99 <yennam20@gmail.com>
2024-11-26 12:52:50 +0530

    Implemented the Add button

--- gui.py
+++ gui.py
@@ -2,10 +2,26 @@ from ctypes import windll
 
 import FreeSimpleGUI as sg
 
+from modules import functions
+
 label=sg.Text("Type in a to-do")
-input_box=sg.InputText(tooltip="Enter todo")
+input_box=sg.InputText(tooltip="Enter todo",key="todo")
 add_button=sg.Button("Add")
 
-window=sg.Window("My To-Do App",layout=[[label],[input_box],[add_button]])
-window.read()
+window=sg.Window("My To-Do App",
+                 layout=[[label],[input_box],[add_button]],
+                 font=("Helvetica",10))
+while True:
+    event, values=window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos=functions.get_todos()
+            new_todo=values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
--- modules/functions.py
+++ modules/functions.py
@@ -1,9 +1,11 @@
-def get_todos(filepath="todo.txt"):
+FILEPATH="todo.txt"
+
+def get_todos(filepath=FILEPATH):
     with open(f'file/{filepath}', 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(filepath,todos_arg):
+def write_todos(todos_arg,filepath=FILEPATH):
     with open(f'file/{filepath}', 'w') as file:
         file.writelines(todos_arg)
 
