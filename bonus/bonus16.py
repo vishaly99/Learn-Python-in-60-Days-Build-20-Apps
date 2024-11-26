963a116e161beac658371d13055a0d1b0434bc10
vishaly99 <yennam20@gmail.com>
2024-11-26 18:42:42 +0530

    Implemented the zip creator

--- bonus/bonus16.py
+++ bonus/bonus16.py
@@ -1,22 +1,35 @@
+from distutils.command.install import value
 
 import FreeSimpleGUI as sg
+from openpyxl.styles.builtins import output
 
+#from gui import event, values
+
+from zip_creator import make_archive
 
 label1=sg.Text("Select the compression:=")
-label2=sg.Text("Select the destination location:=")
+label2=sg.Text("Select the dest location:=")
 
 selected_compression = sg.Input()
 selected_destination = sg.Input()
 
 
-add_button1=sg.FileBrowse("Select Location")
-add_button2=sg.FolderBrowse("Select Destination")
+add_button1=sg.FileBrowse("Select Location",key="files")
+add_button2=sg.FolderBrowse("Select Destination",key="folder")
 compress=sg.Button("Compress")
 
+output_label=sg.Text(Key="output",text_color="green")
 
 window=sg.Window("File compressor",
                  layout=[[label1,selected_compression,add_button1],
                         [label2,selected_destination,add_button2],
-                         [compress]])
-window.read()
+                         [compress,output_label]])
+while True:
+    event, values=window.read()
+    print(event,values)
+    filepaths=values["files"].split(";")
+    folder=values["folder"]
+    make_archive(filepaths,folder)
+    window["output"].update(value="Compression completed!")
+
 window.close()
